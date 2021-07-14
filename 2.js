e3ac7c1ec745cab6307d0df3f40a3d369ccf10fc
Erastus Mauliate <emauliate17@gmail.com>
2021-07-14 11:25:51 +0700

    Update nomor 2 dan 3

--- 2.js
+++ 2.js
@@ -1,41 +1,67 @@
 /**
  * UNIQUE DUPLICATE FINDER
  * =======================
- * 
- * uniqueDuplicateFinder adalah sebuah function yang menerima satu parameter berupa kalimat. 
+ *
+ * uniqueDuplicateFinder adalah sebuah function yang menerima satu parameter berupa kalimat.
  * Function akan memberikan return object yang berisi setiap kumpulan kata-kata unik dan duplicate yang ditemukan dalam kalimat.
- * 
+ *
  * Contoh:
  *   - Input  : uniqueFinder('saya dan SAYA suka makan nasi'))
  *   - Output : {
  *       unique: [ 'dan', 'suka', 'makan', 'nasi' ],
  *       duplicate: [ 'saya' ]
  *     }
- * 
+ *
  * RULES :
  *   - Kerjakan tanpa built-in function kecuali .push(), Number(), String(), .toString(), .toLowerCase(), .toUpperCase().
  */
 
- function stringToArray(str){
-    // code 
+function stringToArray(str) {
+  // code
+  let newStr = str.toLowerCase();
+  let result = [];
+  let temp = "";
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] !== " ") {
+      temp = temp + newStr[i];
+    } else {
+      result.push(temp);
+      temp = "";
+    }
+    if (i === newStr.length - 1) {
+      result.push(temp);
+      temp = "";
+    }
+  }
+  return result;
 }
 
-console.log(stringToArray('saya dan SAYA suka makan nasi'))
+console.log(stringToArray("saya dan SAYA suka makan nasi"));
 // ["saya", "dan", "saya", "suka", "makan", "nasi"]
 
 function uniqueDuplicateFinder(str) {
   // Your code here
+  let convertToArray = stringToArray(str);
+  let result = [];
+  for (let i = 0; i < convertToArray.length; i++) {
+    let temp = convertToArray[i];
+    if (!result[temp]) {
+      result[temp] = "Unique";
+    } else {
+      result[temp] = "duplicate";
+    }
+  }
+  return result;
 }
 
 // Test Case
-console.log(uniqueDuplicateFinder('saya dan SAYA suka makan nasi'));
+console.log(uniqueDuplicateFinder("saya dan SAYA suka makan nasi"));
 // {
 //   unique: ['dan', 'suka', 'makan', 'nasi'],
 //   duplicate: ['saya']
 // }
 
-
-console.log(uniqueDuplicateFinder('dia percaya aku percaya'));
+console.log(uniqueDuplicateFinder("dia percaya aku percaya"));
 // {
 //   unique: ['dia', 'aku'],
 //   duplicate: ['percaya']
--- 3.js
+++ 3.js
@@ -1,11 +1,11 @@
 /**
  * ===============================
  * JYV ENTERTAINMENT INVITATION
- * =============================== 
- * 
+ * ===============================
+ *
  * Seorang pengusaha muda bernama J.Y. Vincent selaku CEO dari JYV Entertainment dapat undangan untuk mengisi acara January K-POP Concert.
  * JYV akan memberikan group dan jumlah performances dari masing-masing group sesuai dengan budget mereka.
- *  
+ *
  * DIRECTION
  * ------------
  * Proses peng-hire an :
@@ -14,7 +14,7 @@
  * - Proses akan berulang jika budget masih cukup
  * - Jika sudah tidak bisa meng-hire karena budget tidak cukup, maka proses kelar
  * - Hasil yang di harapkan berupa object literal
- * 
+ *
  * Contoh :
  * let groups = [
  * {
@@ -27,7 +27,7 @@
  * }
  * ]
  * Budget yang dimiliki adalah 250
- * 
+ *
  * Proses :
  * 1. B-Bang akan di hire karena budget cukup, lalu budget berkurang  100 sesuai harga B-Bang
  * 2. 12yes akan di hire karena budget cukup, lalu budget berkurang  50 sesuai harga 12yes
@@ -36,40 +36,91 @@
  * 5. Ketika budget sudah tidak bisa menghire siapapun dari group, maka proses telah selesai
  * 6. Hasil sisa budget akan ditampilkan
  * 7. Expected result berupa object literal
- * 
+ *
  * Rules : Tidak boleh menggunakan built-in function!
  */
 
- let groups = [
-    {
-        name: "Twoosh",
-        price: 500
-    },
-    {
-        name: "Itsy",
-        price: 175
-    },
-    {
-        name: "GET7",
-        price: 550,
-    },
-    {
-        name: "Whitepink",
-        price: 250
-    },
-    {
-        name: "JYV",
-        price: 300
-    }
-]
-
+let groups = [
+  {
+    name: "Twoosh",
+    price: 500,
+  },
+  {
+    name: "Itsy",
+    price: 175,
+  },
+  {
+    name: "GET7",
+    price: 550,
+  },
+  {
+    name: "Whitepink",
+    price: 250,
+  },
+  {
+    name: "JYV",
+    price: 300,
+  },
+];
 
 function hireGroup(groups, budget) {
-    // code di sini
+  // code di sini
+  let countTotal = 0;
+  let countTwoosh = 0;
+  let countItsy = 0;
+  let countGET7 = 0;
+  let countWhitepink = 0;
+  let countJYV = 0;
 
+  let performances = {}
+  
+  if (budget !== undefined) {
+    for (let i = 0; i < groups.length; i++) {
+      let groupName = groups[i]["name"];
+      let groupPrice = groups[i]["price"];
+
+      switch (groupName) {
+        case "Twoosh":
+          if (budget > groupPrice) {
+            countTwoosh++;
+            budget -= groupPrice;
+          }
+          break;
+        case "Itsy":
+          if (budget > groupPrice) {
+            countItsy++;
+            budget -= groupPrice;
+          }
+          break;
+        case "GET7":
+          if (budget > groupPrice) {
+            countGET7++;
+            budget -= groupPrice;
+          }
+          break;
+        case "Whitepink":
+          if (budget > groupPrice) {
+            countWhitepink++;
+            budget -= groupPrice;
+          }
+          break;
+        case "JYV":
+          if (budget > groupPrice) {
+            countJYV++;
+            budget -= groupPrice;
+          }
+          break;
+      }
+    }
+    countTotal = countTwoosh + countItsy + countGET7 + countWhitepink + countJYV
+    console.log(`${countTwoosh}, ${countItsy}, ${countGET7}, ${countWhitepink}, ${countJYV}`)
+  } else {
+    console.log("There is no budget");
+  }
+  return countTotal
 }
 
-console.log(hireGroup(groups, 5350))
+console.log(hireGroup(groups, 5350));
 // {
 //     performances : {
 //         Twoosh : 3,
@@ -81,7 +132,7 @@ console.log(hireGroup(groups, 5350))
 //     exchange : 25
 // }
 
-console.log(hireGroup(groups, 1000))
+console.log(hireGroup(groups, 1000));
 // {
 //     performances : {
 //         Twoosh : 1,
@@ -90,17 +141,17 @@ console.log(hireGroup(groups, 1000))
 //     },
 //     exchange : 75
 // }
-console.log(hireGroup(groups, 350))
+console.log(hireGroup(groups, 350));
 // {
 //     performances : {
 //         Itsy : 2,
 //     },
 //     exchange : 0
 // }
-console.log(hireGroup(groups, 100))
+console.log(hireGroup(groups, 100));
 // {
 //     performances :  {}
 //     exchange : 100
 // }
-console.log(hireGroup(groups))
+console.log(hireGroup(groups));
 // There's no budget.
